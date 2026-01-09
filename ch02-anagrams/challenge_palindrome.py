36cccd67e17f13844fd0559db22e4c1a89cb8e7b
Brian Strauss <bstrauss@umass.edu>
2026-01-09 08:31:32 -0500

    update

---
+++ ch02-anagrams/challenge_palindrome.py
@@ -0,0 +1,14 @@
+'''
+Find palindromes
+Palindromes are pairs of words that become each other when we reverse the order of their letters. That means that all palindromes are anagrams!
+
+Your task: Write a function that returns all palindromes of seven letters or more. You will work with the dictionary of anagram sets indexed by signature that we created in video 02_03.
+
+Parameters
+anagrams_by_signature: A dictionary containing sets of anagrams, indexed by their signature
+
+Result
+A list of set objects containing all palindromic pairs of seven letters or more
+'''
+
+print('hi')
